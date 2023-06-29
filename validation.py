de30cdf334c76ba868039bd8cfebb6f2c6413555
shotomorisaki <jojoto8845@icloud.com>
2023-06-29 16:18:05 +0900

    Modification 6/29

--- validation.py
+++ validation.py
@@ -14,3 +14,22 @@ class Validation:
     def div(a, b):
         return a / b
     
+    def is_prime(n: int) -> bool:
+        if n <= 1:
+            return False
+
+        if n == 2:
+            return True
+
+        if n % 2 == 0:
+            return False
+
+        i = 3
+
+        while i * i <= n:
+            if n % i == 0:
+                return False
+
+            i += 2
+
+        return True
